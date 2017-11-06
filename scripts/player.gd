c914da39063e4b2a319b57f39c157d9ba3b2cc86
cocosar07 <cocosar07370@gmail.com>
2017-11-06 21:55:52 +0100

    Start player movements

---
+++ scripts/player.gd
@@ -0,0 +1,30 @@
+extends KinematicBody2D
+
+export(float) var max_speed = 120
+export(float) var acceleration = 400
+
+var current_speed = 0
+
+func _ready():
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	var velocity = Vector2()
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+
+	if velocity.x != 0 || velocity.y != 0:
+		current_speed += acceleration * delta
+		if current_speed > max_speed:
+			current_speed = max_speed
+
+	move(velocity * current_speed * delta)
+
+func _draw():
+	draw_rect(Rect2(-32, -32, 64, 64), Color(1, 1, 1))
