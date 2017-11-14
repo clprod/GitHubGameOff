8c0520da91fc770af19f55aab1fae3a2dc35311d
1e0 <leolafiosca@gmail.com>
2017-11-14 21:54:00 +0100

    Portal is now glowing

---
+++ scripts/glow.gd
@@ -0,0 +1,21 @@
+extends Sprite
+
+# class member variables go here, for example:
+# var a = 2
+var decrease = true
+
+func _ready():
+	set_opacity(1)
+	set_fixed_process(true)
+
+func _fixed_process(delta):
+	print(get_opacity())
+	if decrease:
+		set_opacity(get_opacity()-0.01)
+		if get_opacity() <= 0:
+			decrease = false
+	else:
+		set_opacity(get_opacity()+0.01)
+		if get_opacity() == 1:
+			decrease = true
+	
